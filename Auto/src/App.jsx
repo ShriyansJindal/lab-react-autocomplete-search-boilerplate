47f4368bdefecd3b0abf38763dbdbb911ceafeb6
shriyans jindal <shriyansjindal40517@gmail.com>
2024-01-22 15:22:43 +0530

    done

---
+++ Auto/src/App.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react'
+import countryData from "./resources/countryData.json" 
+import './App.css'
+
+function App() {
+  const [text, setText] = useState("");
+  const [suggestion, setSuggestion] = useState([]);
+  const [showsuggestion, setShowsuggestion] = useState(true)
+  // console.log(countryData)
+
+  function handlechange(e){
+    setText(e.target.value)
+
+    setSuggestion(countryData.filter((el)=>el.name.toLowerCase().startsWith(text.toLowerCase())))
+    setShowsuggestion(true)
+
+  }
+  let handleEsc =(e)=>{
+    if(e.keyCode==27){
+      setShowsuggestion(false)
+      console.log('escape')
+    } 
+  }
+  return (
+    <div  >
+     <input type="text" value={text} onChange={handlechange} onKeyDown={handleEsc}  list='suggest' />
+     <datalist id='suggest' >
+      {showsuggestion && suggestion.map((el,i)=>(
+        <option key={i} value={el.name}></option>
+      ))}
+     </datalist>
+    </div>
+  )
+}
+
+export default App
